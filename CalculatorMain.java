17a36de2b9009c9870d09b9a32e5f155f57c8e0c
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2018-09-17 19:16:08 -0400

    Add files via upload

---
+++ CalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+	
+		new Calculator();
+		Calculator calc = new Calculator();
+		System.out.print(calc.add (2,6));
+		System.out.print("\nThe difference between 2 and 6 is "+ calc.sub(2,6));
+	}
+
+}
